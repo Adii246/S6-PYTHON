61774f0307eb6a37af97202e44d2c012d0bbe71f
ADITHYA RAJ <145443565+Adii246@users.noreply.github.com>
2025-01-15 10:10:09 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,80 @@
+'''
+x= 10
+x="asdi"
+print(x)
+
+
+n=[2,5,9,6,3]
+print("sum of the numbers is ",sum(n))
+
+import math
+print(math.sqrt(100))
+
+if x=="asdi":
+    print("true")
+
+print("it\'s me")
+
+print("\u2764")
+print("\U0001F600")
+print("\u6512")
+print("\U00065D42")
+
+m=("i"+"il ") *5
+print(m)
+print("piku "*3)
+
+a="aDIthya raj lidiya reavtahy"
+print(a[1:3])
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.title())
+print(a.swapcase())
+print(len(a))
+
+
+print(a.find("raj"))
+print(a.find("diya"))
+print(a.replace("reavtahy","revz"))
+print(a)
+print(a.count("a"))
+print("\n")
+
+print("ASSIGNEMENT 1!!")
+print("\n")
+q="I LOVE TRAVELLING"
+print(q.split())
+w="adi"
+g="greem "
+print(g.join(g))
+
+s="-*--adithya raj----***"
+print("s=",s)
+print(s.strip('*'))
+print(s.lstrip("-"))
+print(s.rstrip("*"))      
+
+s="my name is {}"
+print("lobe ke {f}".format(f='adi'))
+print("llolo ke {1}".format('lidi','rev','adi'))
+
+
+r='5ad'
+print(r.isalpha())
+print(r.isdigit())
+print(r.isspace())
+
+
+l="adithyarak lididjij huisdfiugsf"
+print(l.startswith("ra",7,12))
+
+
+
+
+
+
+
+
+
+
